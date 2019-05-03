1f15e93015d59cbf918058e9bf36950d020c58d8
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-03 01:54:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,24 +1,26 @@
 def using_include(array, element)
-  array.include?(element)
+  return array.include?(element)
 
 end
 
 def using_sort(array)
-  array.sort!
+  return array.sort!
 end
 
 def using_reverse(array)
+  return array.reverse
 
 end
 
 def using_first(array)
+  return array.first
 
 end
 
 def using_last(array)
-
+  return array.last
 end
 
 def using_size(array)
-
+  return array.size
 end
